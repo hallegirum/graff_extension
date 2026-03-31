c294212beccc076bc1525e7ed356b9b6ccf85240
hallegirum <halleluiah_girum@brown.edu>
2026-03-31 19:44:56 +0000

    data

--- src/graph_rewire.py
+++ src/graph_rewire.py
@@ -1,7 +1,7 @@
 ## gradient-based rewiring
 from itertools import product
 import torch
-from torch_geometric.utils import get_laplacian, remove_self_loops, add_remaining_self_loops, contains_self_loops
+from torch_geometric.utils import get_laplacian, remove_self_loops, add_remaining_self_loops, contains_self_loops, is_undirected
 import torch_sparse
 
 
@@ -32,8 +32,32 @@ def bottleneck_score(LX_i, LX_j,eps=1e-08, alpha=1, beta=1):
   score = alpha * grad_diff + beta * mag
   return score 
 
+def evaluate_neighbourhood(X, edge_index, n, local_neigh, eta=0.1):
+    LX = dirichlet_energy_grad(edge_index, n, X)
+    one_step_diffusion = X - eta * LX
+    LX_new = dirichlet_energy_grad(edge_index, n, one_step_diffusion)
 
-def energy_gradient_rewire(edge_index, n, X, k):
+    local_score = 0.0
+    count = 0
+    seen = set()
+    local_set = set(local_neigh)
+    for u in local_neigh:
+        for v in get_neighbours(edge_index, u):
+            if v in local_set:
+                a, b = sorted((u, v))
+                if (a, b) in seen:
+                    continue
+                seen.add((a, b))
+
+                local_score += bottleneck_score(LX_new[u], LX_new[v])
+                count += 1
+
+    if count == 0:
+        return 0.0
+
+    return local_score / count
+
+def energy_gradient_rewire(edge_index, n, X, k,eta=0.1):
 
   """
   edge_index : (2, m) indicating that there is an edge between node i and node j
@@ -42,13 +66,16 @@ def energy_gradient_rewire(edge_index, n, X, k):
   k : number of edges add
   adj : (n,n) adjacency matrix
   """
+  print(edge_index.shape)
   for idx in range(k):
     edge_index, _ = remove_self_loops(edge_index)
     LX = dirichlet_energy_grad(edge_index,n,X)
-    score = bottleneck_score(LX[edge_index[0]],LX[edge_index[1]])
-    print("min:", score.min().item())
-    print("max:", score.max().item())
-    print("std:", score.std().item())
+    X_prop = X - eta * LX
+    LX_prop = dirichlet_energy_grad(edge_index, n, X_prop)
+    score = bottleneck_score(LX_prop[edge_index[0]],LX_prop[edge_index[1]])
+    # print("min:", score.min().item())
+    # print("max:", score.max().item())
+    # print("std:", score.std().item())
     bottleneck= torch.argmin(score,dim=-1)
     node_i = edge_index[0,bottleneck].item()
     node_j = edge_index[1,bottleneck].item()
@@ -56,32 +83,37 @@ def energy_gradient_rewire(edge_index, n, X, k):
     print("node_j",node_j)
     neigh_i = get_neighbours(edge_index,node_i)
     neigh_j = get_neighbours(edge_index,node_j)
-    print("neighboursi", len(neigh_i))
-    print("neighboursj",len(neigh_j))
+    # print("neighboursi", len(neigh_i))
+    # print("neighboursj",len(neigh_j))
 
     max_score = (-float('inf'),(-1,-1))
-    print("old_score", bottleneck_score(LX[node_i],LX[node_j]))
+    print("old_bn", score.min())
+    local_neigh = list(set(neigh_i + neigh_j + [node_i, node_j]))
+    old_score = evaluate_neighbourhood(X,edge_index,n,local_neigh)
     for (u,v) in product(neigh_i,neigh_j):
 
         if has_edge(edge_index,u,v) or u==v:
           continue
         rewired_edge_index = add_edge(edge_index,u,v)
-        LX_new = dirichlet_energy_grad(rewired_edge_index,n,X)
-        score_ij = bottleneck_score(LX_new[node_i],LX_new[node_j])
-        print("new_score",score_ij)
-        if max_score[0] < score_ij:
-          max_score = (score_ij, (u,v))
+        new_score = evaluate_neighbourhood(X,rewired_edge_index,n,local_neigh)
+        relief = new_score - old_score
+        # print("old_score",old_score)
+        # print("new_score",new_score)
+        if max_score[0] < relief:
+          max_score = (relief, (u,v))
 
 
     if max_score[1] != (-1,-1):
-      print("new_edge",max_score[0])
+      print("edge_added")
       u,v = max_score[1]
       edge_index = add_edge(edge_index,u,v)
       edge_index = add_edge(edge_index,v,u)
+      # LX_new = dirichlet_energy_grad(edge_index,n,X)
+      # X = X - eta *LX_new
 
   edge_index, _ = add_remaining_self_loops(edge_index)
+  print(edge_index.shape)
   return edge_index
-
 
 
 
